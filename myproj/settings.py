1f55e6e003645bb2fc31c5f2054884a5ad3845ef
Mukhter Hossain <mukhos1@gmail.com>
2022-01-13 13:09:10 +0600

    commit after migration

--- myproj/settings.py
+++ myproj/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-5!=r3qszp#lyebisa&u_it3sql+j=-ysiu9ul*8pjrw&^9i716'
-
+#SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
